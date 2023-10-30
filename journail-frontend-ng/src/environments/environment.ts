bdc64c0ae42f37e25e9fdca0df4369d962186c04
maelissteissier <maeliss.teissier@gmail.com>
2023-10-30 00:10:39 -0400

    config

--- journail-frontend-ng/src/environments/environment.ts
+++ journail-frontend-ng/src/environments/environment.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://localhost:5000'
+// };
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000'
+  production: true,
+  apiUrl: ''
 };
